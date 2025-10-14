f9ca3ad2cbecd46c2e2f3d140a27c04429875a9b
junaid-repo <junaidraza3001@gmail.com>
2025-10-14 19:35:00 +0530

    updates

--- shop/src/main/java/com/management/shop/repository/BillingRepository.java
+++ shop/src/main/java/com/management/shop/repository/BillingRepository.java
@@ -136,18 +136,10 @@ public interface BillingRepository extends JpaRepository<BillingEntity, Integer>
             "  SELECT " +
             "    DATE(bp.created_date) AS sale_date, " +
             "    SUM(bd.total_amount) AS daily_total, " +
-            "    SUM(bd.total_profit_oncp) AS daily_profit " +
+            "    SUM(bd.total_profit_oncp) AS daily_profit, " +
+            "    SUM(bd.units_sold) AS daily_stocks_sold " + // Added this line
             "  FROM billing_payments bp " +
             "  JOIN billing_details bd ON bp.billing_id = bd.id " +
-            "  WHERE bp.user_id = :userId AND bp.created_date >= DATE_SUB(:currentDateMax, INTERVAL 7 DAY) " +
-            "  GROUP BY sale_date " +
-            "), " +
-            "daily_stocks AS ( " +
-            "  SELECT " +
-            "    DATE(bp.created_date) AS sale_date, " +
-            "    SUM(ps.quantity) AS daily_stocks_sold " +
-            "  FROM billing_payments bp " +
-            "  JOIN product_sales ps ON bp.id = ps.billing_id " +
             "  WHERE bp.user_id = :userId AND bp.created_date >= DATE_SUB(:currentDateMax, INTERVAL 7 DAY) " +
             "  GROUP BY sale_date " +
             ") " +
@@ -155,10 +147,9 @@ public interface BillingRepository extends JpaRepository<BillingEntity, Integer>
             "  DATE_FORMAT(d.d, '%a') AS day, " +
             "  COALESCE(ds.daily_total, 0) AS count, " +
             "  COALESCE(ds.daily_profit, 0) AS totalProfit, " +
-            "  COALESCE(d_stocks.daily_stocks_sold, 0) AS totalStocksSold " +
+            "  COALESCE(ds.daily_stocks_sold, 0) AS totalStocksSold " + // Updated to use 'ds' alias
             "FROM days d " +
-            "LEFT JOIN daily_sales ds ON d.d = ds.sale_date " +
-            "LEFT JOIN daily_stocks d_stocks ON d.d = d_stocks.sale_date " +
+            "LEFT JOIN daily_sales ds ON d.d = ds.sale_date " + // Removed the join to daily_stocks
             "ORDER BY d.d",
             nativeQuery = true)
     List<Object[]> getWeeklySalesAndStocks(@Param("currentDateMax") LocalDateTime currentDateMax,
@@ -221,25 +212,10 @@ public interface BillingRepository extends JpaRepository<BillingEntity, Integer>
             "      ELSE 20 " +
             "    END AS slot_start_hour, " +
             "    SUM(bd.total_amount) AS hourly_total, " +
-            "    SUM(bd.total_profit_oncp) AS hourly_profit " +
+            "    SUM(bd.total_profit_oncp) AS hourly_profit, " +
+            "    SUM(bd.units_sold) AS hourly_stocks_sold " + // Fetches stocks from billing_details
             "  FROM billing_payments bp " +
             "  JOIN billing_details bd ON bp.billing_id = bd.id " +
-            "  WHERE bp.user_id = :userId AND DATE(bp.created_date) = DATE(:currentDate) " +
-            "  GROUP BY slot_start_hour " +
-            "), " +
-            "hourly_stocks AS ( " +
-            "  SELECT " +
-            "    CASE " +
-            "      WHEN HOUR(bp.created_date) BETWEEN 0 AND 3 THEN 0 " +
-            "      WHEN HOUR(bp.created_date) BETWEEN 4 AND 7 THEN 4 " +
-            "      WHEN HOUR(bp.created_date) BETWEEN 8 AND 11 THEN 8 " +
-            "      WHEN HOUR(bp.created_date) BETWEEN 12 AND 15 THEN 12 " +
-            "      WHEN HOUR(bp.created_date) BETWEEN 16 AND 19 THEN 16 " +
-            "      ELSE 20 " +
-            "    END AS slot_start_hour, " +
-            "    SUM(ps.quantity) AS hourly_stocks_sold " +
-            "  FROM billing_payments bp " +
-            "  JOIN product_sales ps ON bp.id = ps.billing_id " +
             "  WHERE bp.user_id = :userId AND DATE(bp.created_date) = DATE(:currentDate) " +
             "  GROUP BY slot_start_hour " +
             ") " +
@@ -247,42 +223,30 @@ public interface BillingRepository extends JpaRepository<BillingEntity, Integer>
             "  ts.slot_label AS timeOfDay, " +
             "  COALESCE(hs.hourly_total, 0) AS count, " +
             "  COALESCE(hs.hourly_profit, 0) AS totalProfit, " +
-            "  COALESCE(h_stocks.hourly_stocks_sold, 0) AS totalStocksSold " +
+            "  COALESCE(hs.hourly_stocks_sold, 0) AS totalStocksSold " + // Uses the new column from hourly_sales
             "FROM time_slots ts " +
             "LEFT JOIN hourly_sales hs ON ts.start_hour = hs.slot_start_hour " +
-            "LEFT JOIN hourly_stocks h_stocks ON ts.start_hour = h_stocks.slot_start_hour " +
             "ORDER BY ts.start_hour",
             nativeQuery = true)
     List<Object[]> getSalesAndStocksToday(@Param("currentDate") LocalDateTime currentDate,
-                                                @Param("userId") String userId);
+                                          @Param("userId") String userId);
 
-    @Query(value = "WITH weeks AS ( " +
-            "  SELECT DATE(:currentDateMax) AS week_end_date " +
+    @Query(value = "WITH RECURSIVE weeks (week_end_date) AS ( " +
+            "  SELECT DATE(:currentDateMax) " +
             "  UNION ALL " +
-            "  SELECT DATE(DATE_SUB(:currentDateMax, INTERVAL 1 WEEK)) " +
-            "  UNION ALL " +
-            "  SELECT DATE(DATE_SUB(:currentDateMax, INTERVAL 2 WEEK)) " +
-            "  UNION ALL " +
-            "  SELECT DATE(DATE_SUB(:currentDateMax, INTERVAL 3 WEEK)) " +
+            "  SELECT DATE(DATE_SUB(week_end_date, INTERVAL 1 WEEK)) " +
+            "  FROM weeks " +
+            "  WHERE DATE_SUB(week_end_date, INTERVAL 1 WEEK) >= DATE_SUB(:currentDateMax, INTERVAL 1 MONTH) " +
             "), " +
             "weekly_sales AS ( " +
             "  SELECT " +
             "    w.week_end_date, " +
             "    SUM(bd.total_amount) AS total, " +
-            "    SUM(bd.total_profit_oncp) AS profit " +
+            "    SUM(bd.total_profit_oncp) AS profit, " +
+            "    SUM(bd.units_sold) AS stocks_sold " +
             "  FROM weeks w " +
             "  JOIN billing_payments bp ON DATE(bp.created_date) BETWEEN DATE_SUB(w.week_end_date, INTERVAL 6 DAY) AND w.week_end_date " +
             "  JOIN billing_details bd ON bp.billing_id = bd.id " +
-            "  WHERE bp.user_id = :userId " +
-            "  GROUP BY w.week_end_date " +
-            "), " +
-            "weekly_stocks AS ( " +
-            "  SELECT " +
-            "    w.week_end_date, " +
-            "    SUM(ps.quantity) AS stocks_sold " +
-            "  FROM weeks w " +
-            "  JOIN billing_payments bp ON DATE(bp.created_date) BETWEEN DATE_SUB(w.week_end_date, INTERVAL 6 DAY) AND w.week_end_date " +
-            "  JOIN product_sales ps ON bp.id = ps.billing_id " +
             "  WHERE bp.user_id = :userId " +
             "  GROUP BY w.week_end_date " +
             ") " +
@@ -290,14 +254,13 @@ public interface BillingRepository extends JpaRepository<BillingEntity, Integer>
             "  DATE_FORMAT(w.week_end_date, '%b %d') AS period, " +
             "  COALESCE(ws.total, 0) AS count, " +
             "  COALESCE(ws.profit, 0) AS totalProfit, " +
-            "  COALESCE(wst.stocks_sold, 0) AS totalStocksSold " +
+            "  COALESCE(ws.stocks_sold, 0) AS totalStocksSold " +
             "FROM weeks w " +
             "LEFT JOIN weekly_sales ws ON w.week_end_date = ws.week_end_date " +
-            "LEFT JOIN weekly_stocks wst ON w.week_end_date = wst.week_end_date " +
             "ORDER BY w.week_end_date ASC",
             nativeQuery = true)
     List<Object[]> getSalesAndStocksMonthly(@Param("currentDateMax") LocalDateTime currentDateMax,
-                                                  @Param("userId") String userId);
+                                            @Param("userId") String userId);
 
     @Query(value = "WITH RECURSIVE months (month_start) AS ( " +
             "  SELECT DATE(DATE_FORMAT(DATE_SUB(:currentDateMax, INTERVAL 11 MONTH), '%Y-%m-01')) " +
@@ -308,20 +271,10 @@ public interface BillingRepository extends JpaRepository<BillingEntity, Integer>
             "  SELECT " +
             "    DATE(DATE_FORMAT(bp.created_date, '%Y-%m-01')) AS month_period, " +
             "    SUM(bd.total_amount) AS total, " +
-            "    SUM(bd.total_profit_oncp) AS profit " +
+            "    SUM(bd.total_profit_oncp) AS profit, " +
+            "    SUM(bd.units_sold) AS stocks_sold " + // Added this line
             "  FROM billing_payments bp " +
             "  JOIN billing_details bd ON bp.billing_id = bd.id " +
-            "  WHERE bp.user_id = :userId " +
-            "    AND bp.created_date >= DATE(DATE_FORMAT(DATE_SUB(:currentDateMax, INTERVAL 11 MONTH), '%Y-%m-01')) " +
-            "    AND bp.created_date <= :currentDateMax " +
-            "  GROUP BY month_period " +
-            "), " +
-            "monthly_stocks AS ( " +
-            "  SELECT " +
-            "    DATE(DATE_FORMAT(bp.created_date, '%Y-%m-01')) AS month_period, " +
-            "    SUM(ps.quantity) AS stocks_sold " +
-            "  FROM billing_payments bp " +
-            "  JOIN product_sales ps ON bp.id = ps.billing_id " +
             "  WHERE bp.user_id = :userId " +
             "    AND bp.created_date >= DATE(DATE_FORMAT(DATE_SUB(:currentDateMax, INTERVAL 11 MONTH), '%Y-%m-01')) " +
             "    AND bp.created_date <= :currentDateMax " +
@@ -331,14 +284,13 @@ public interface BillingRepository extends JpaRepository<BillingEntity, Integer>
             "  DATE_FORMAT(m.month_start, '%b %Y') AS period, " +
             "  COALESCE(ms.total, 0) AS count, " +
             "  COALESCE(ms.profit, 0) AS totalProfit, " +
-            "  COALESCE(mst.stocks_sold, 0) AS totalStocksSold " +
+            "  COALESCE(ms.stocks_sold, 0) AS totalStocksSold " + // Updated to use 'ms' alias
             "FROM months m " +
-            "LEFT JOIN monthly_sales ms ON m.month_start = ms.month_period " +
-            "LEFT JOIN monthly_stocks mst ON m.month_start = mst.month_period " +
+            "LEFT JOIN monthly_sales ms ON m.month_start = ms.month_period " + // Removed the second join
             "ORDER BY m.month_start ASC",
             nativeQuery = true)
     List<Object[]> getSalesAndStocksYearly(@Param("currentDateMax") LocalDateTime currentDateMax,
-                                                  @Param("userId") String userId);
+                                           @Param("userId") String userId);
 
     @Query(
             value = "SELECT * " +
